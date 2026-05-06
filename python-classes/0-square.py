014592e5f389cbd4800c12da739ef4630b8acffa
Fernando Biesdorf <biesdorf@gmail.com>
2026-05-06 17:03:58 +0200

    added some documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 class Square:
-    """ This class defines a square, and does not do much more"""
+    """ This class defines a square, and does not do much more
+
+    The class takes no input, and no fields, and does nothing
+    Args:
+        none
+    """
 
     pass

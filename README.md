530d99714abe68cb125d7f114d60ecef682bf89f
Fernando Biesdorf <biesdorf@gmail.com>
2026-05-06 16:52:46 +0200

    Exercise 0

---
+++ README.md
@@ -0,0 +1,1 @@
+This project folder is about classes
---
+++ python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """ This class defines a square, and does not do much more"""
+
+    def __init__(self):
+        """This Method initializes a Square object"""
+        pass

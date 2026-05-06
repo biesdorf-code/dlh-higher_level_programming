f9fb7b3047b18bd8d75f0eea9284cd25b77870f2
Fernando Biesdorf <biesdorf@gmail.com>
2026-05-06 17:10:24 +0200

    0, commented the module

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+""" This module contains a test Class called Square, nothing alse
+
+This module provides a simple example that can be tested
+"""
+
+
 class Square:
     """ This class defines a square, and does not do much more
 

c695edc96b28f987bbb8ed79e64fc245079d15bf
Fernando Biesdorf <biesdorf@gmail.com>
2026-05-06 17:43:00 +0200

    2. raise raise

---
+++ python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" This module contains a Square class
+
+It is a simple class just used for learning
+"""
+
+
+class Square:
+    """ This class holds the area
+
+    The class holds it's private part: area
+    """
+
+    def __init__(self, size=0):
+        """This method initializes the instance
+
+        This is when the square learn its size for all eternity
+        Args:
+            size (int): is the desired size, 0 if unknown
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    self.__size = size

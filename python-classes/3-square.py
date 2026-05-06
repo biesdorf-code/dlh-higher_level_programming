8d75aa34e1f718091d2afa589419eae1098e2947
Fernando Biesdorf <biesdorf@gmail.com>
2026-05-06 18:00:06 +0200

    3. area

---
+++ python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" This module contains a Square class
+
+It is a simple class just used for learning
+"""
+
+
+class Square:
+    """ This class holds the area
+
+    The class holds it's private part: area
+    """
+
+    def __init__(self, size=0):
+        """This method initializes the instance
+
+        This is when the square learn its size for all eternity
+        Args:
+            size (int): is the desired size, 0 if unknown
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        #: protected variable, int
+        self.__size = size
+
+    def area(self):
+        """ Area of this instance of square
+
+        The method gives access to the area based on the size
+        """
+        return self.__size**2

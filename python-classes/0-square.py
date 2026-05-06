994c473c3f1870be1420a296cea04c782356af64
Fernando Biesdorf <biesdorf@gmail.com>
2026-05-06 16:56:49 +0200

    bit of the same

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,6 +2,4 @@
 class Square:
     """ This class defines a square, and does not do much more"""
 
-    def __init__(self):
-        """This Method initializes a Square object"""
-        pass
+   pass

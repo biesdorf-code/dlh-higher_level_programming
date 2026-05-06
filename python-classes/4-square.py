3a96fea0c2575ffdf71403933e356c4884285063
Fernando Biesdorf <biesdorf@gmail.com>
2026-05-06 19:02:06 +0200

    4, after @properties

---
+++ python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" This module contains a Square class"""
+
+
+class Square:
+    """ This class holds the area"""
+
+    def __init__(self, size=0):
+        """This method initializes the instance"""
+
+        self.size = size
+
+    @property
+    def size(self):
+        """" this method is used to retrieve the size, private"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets this attribute, raises exceptions as needed"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Area of this instance of square"""
+        return self.size ** 2

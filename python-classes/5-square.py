b19a24302294f5c118761f248a5e881c9a1ca31d
Fernando Biesdorf <biesdorf@gmail.com>
2026-05-06 19:13:46 +0200

    5. for for #

---
+++ python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" This module contains a Square class"""
+
+
+class Square:
+    """ This class holds the area"""
+
+    def __init__(self, size=0):
+        """This method initializes the instance"""
+
+        self.size = size
+
+    @property
+    def size(self):
+        """" this method is used to retrieve the size, private"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets this attribute, raises exceptions as needed"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Area of this instance of square"""
+        return self.size ** 2
+
+    def my_print(self):
+        """prints a rectangle that should look like a # square"""
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                for j in range(self.size):
+                    print("#", end="")
+                print()

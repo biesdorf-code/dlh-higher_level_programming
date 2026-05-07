ed5aace147c4aef34b0ae14b5abc68d00e9284d7
Fernando Biesdorf <biesdorf@gmail.com>
2026-05-07 17:52:13 +0200

    9.

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -82,3 +82,8 @@ class Rectangle:
         if rect_2.area() > rect_1.area():
             return rect_2
         return rect_1
+
+    @classmethod
+    def square(cls, size=0):
+        """returns a square sized RECTAGLE Object"""
+        return cls(size, size)  # using Rectangle(size,size) would not be inheritance friendly

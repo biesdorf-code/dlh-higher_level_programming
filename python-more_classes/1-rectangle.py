655c28152342d718dca4ccc3320848a2362964dd
Fernando Biesdorf <biesdorf@gmail.com>
2026-05-07 14:21:19 +0200

    task 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""This module holds 1 class for task 0"""
+
+
+class Rectangle:
+    """ this is a classy class that could also represent a Square, will see
+
+    """
+
+    def __init__(self, width=0, height=0):
+        """ Initiates the rectangle"""
+        self.width = width
+        self.heigth = height
+
+    @property
+    def width(self):
+        """ Getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter that throw exceptions as needed """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter that throw exceptions as needed """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = value

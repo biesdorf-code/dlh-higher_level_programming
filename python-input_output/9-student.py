a01cfc4658ccc1e28ee5952213d2cb32a9a2a164
Fernando Biesdorf <biesdorf@gmail.com>
2026-05-08 14:00:04 +0200

    9. 1st Student class

---
+++ python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module contains a student class"""
+
+
+class Student:
+    """Student that can be initialized and serialized"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns a dictionary of student attributes"""
+        return self.__dict__

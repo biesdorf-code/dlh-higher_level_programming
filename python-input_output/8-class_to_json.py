60ebccd8d7dbb3dcfe1359de2332c8b5f754c8ca
Fernando Biesdorf <biesdorf@gmail.com>
2026-05-08 13:44:59 +0200

    8. __dict\__

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module: __init__ is a JSON serializer of sorts"""
+
+
+def class_to_json(obj):
+    """functions __init__ is a JSON serializer of sorts"""
+    return obj.__dict__

fa6af529dea08a1f5a2468f8edd7c86edd520046
Fernando Biesdorf <biesdorf@gmail.com>
2026-05-10 22:17:28 +0200

    2. 1-line CVS to JSON

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A module for CSV to JSON"""
+import csv
+import json
+
+
+def convert_csv_to_json(csv_filename):
+    """Convert a CSV file to JSON """
+    try:
+        with open(csv_filename, 'r') as f:
+            reader = csv.DictReader(f)
+            data = list(reader)
+
+        with open('data.json', 'w') as f:
+            json.dump(data, f, indent=4)
+
+        return True
+    except FileNotFoundError:
+        return False
